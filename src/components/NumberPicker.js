67c4ea3e3e593f7150dac237cd8e1559d5bb7f86
lanners-marshall <lanners.marshall@gmail.com>
2019-05-06 13:53:54 -0700

    numberpicker

--- src/components/NumberPicker.js
+++ src/components/NumberPicker.js
@@ -3,12 +3,34 @@ import React from 'react'
 class NumberPicker extends React.Component {
 	constructor(){
 		super()
-		this.state = {}
+		this.state = {
+			number: 15
+		}
 	}
 
+	up = () => {
+		const {number} = this.state
+		this.setState({number: number + 1})
+	}
+
+	down = () => {
+		const {number} = this.state
+		this.setState({number: number - 1})
+	}
+
+
 	render(){
+
+		const {number} = this.state
+
 		return(
-			<div>NumberPicker</div>
+			<div className="flexSection">
+				<div>{number}</div>
+				<div className="flexBox marginLeft">
+					<div onClick={this.down}>down</div>
+					<div onClick={this.up} className="marginLeft">up</div>
+				</div>
+			</div>
 		)
 	}
 }
