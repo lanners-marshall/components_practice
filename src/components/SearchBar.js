d889b4dcf673c7cd32f0cac98e5d81c1e284046b
lanners-marshall <lanners.marshall@gmail.com>
2019-05-05 17:20:01 -0700

    first commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			search: ''
+		}
+	}
+
+	handleText = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+
+	render(){
+
+		const {names} = this.props
+		const {search} = this.state
+
+		return (
+			<div>
+				<input 
+					name="search"
+					value={search}
+					onChange={this.handleText}
+					autoComplete="off"
+				/>
+				{search && <ul>
+					{names.filter(name => { return name.toLowerCase().includes(search.toLowerCase())}).map((name, i) => {
+						return <li key={i}>{name}</li>
+					})}
+					</ul>
+				}
+			</div>
+
+		)
+	}
+}
+
+export default SearchBar
